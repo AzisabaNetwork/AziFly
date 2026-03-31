5b40a4753162df161781abac19443c9b102bfc54
albardoo02 <akshs@outlook.jp>
2026-03-31 21:08:15 +0900

    refactor: remove runServer task from build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,15 +31,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.13")
-    }
-}
-
 val targetJavaVersion = 8
 kotlin {
     jvmToolchain(targetJavaVersion)
